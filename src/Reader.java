abc278139f1459f3061822515c8359cfb1a8907f
ecrinozcurass <s35789@pjwstk.edu.pl>
2026-03-17 13:15:35 +0100

    Add Reader class

---
+++ src/Reader.java
@@ -0,0 +1,26 @@
+public class Reader {
+    private String firstName;
+    private String lastName;
+    private int cardNumber;
+    private int borrowedCount;
+     public Reader(String firstName, String lastName, int cardNumber, int borrowedCount){
+         this.firstName = firstName;
+         this.lastName = lastName;
+         this.cardNumber = cardNumber;
+         this.borrowedCount = borrowedCount;
+         this.borrowedCount = 0;
+     }
+     public void printData() {
+         System.out.println(firstName+ " " + lastName);
+         System.out.println("Card number: " + cardNumber);
+         System.out.println("Borrowed books: " + borrowedCount);
+     }
+     public void increaseBorrowedCount(){
+         borrowedCount++;
+     }
+     public void decreaseBorrowedCount(){
+         if (borrowedCount > 0) {
+             borrowedCount--;
+         }
+     }
+}
